de28b96ed25400ce2589c1256c1918527a5e0593
Luckey <luckeysherpa07@gmail.com>
2019-06-18 12:58:34 +0545

    added customized typescirpt tab view to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,44 @@
 import * as React from 'react';
+import { ReactNode } from 'react';
 import { StyleSheet, Dimensions, View } from 'react-native';
-import { TabView, SceneMap } from 'react-native-tab-view';
+import { TabView, SceneMap, SceneRendererProps, TabBar } from 'react-native-tab-view';
+
+export type Key = { key: string }
+
+export type RouteBase = Key & { testID?: string }
+
+export type NavigationState<T extends Key> = {
+  index: number
+  routes: T[]
+}
+
+export type PagerProps = {
+  animationEnabled?: boolean
+  swipeEnabled?: boolean
+  swipeDistanceThreshold?: number
+  swipeVelocityThreshold?: number
+  children?: ReactNode
+}
+
+export type Layout = {
+  height: number
+  width: number
+}
+
+export type SceneRendererProps<T extends RouteBase = RouteBase> = {
+  layout: Layout & {
+    measured: boolean
+  }
+  navigationState: NavigationState<T>
+}
+
+export type TabViewProps<
+  T extends RouteBase = RouteBase
+  > = PagerProps & {
+    navigationState: NavigationState<T>
+    tabBarPosition?: 'bottom' | 'top'
+    renderTabBar?: (props: SceneRendererProps) => ReactNode
+  }
 
 const FirstRoute = () => (
   <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
@@ -10,7 +48,19 @@ const SecondRoute = () => (
   <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
 );
 
-export default class App extends React.Component {
+const renderNewTab = (props: SceneRendererProps & {
+  navigationState: NavigationState<{
+    key: string;
+    title: string;
+  }>;
+}) => <TabBar
+    {...props}
+    indicatorStyle={styles.indicator}
+    style={styles.tabView}
+    labelStyle={styles.label}
+  />
+
+export default class App extends React.Component<TabViewProps> {
   state = {
     index: 0,
     routes: [
@@ -29,6 +79,7 @@ export default class App extends React.Component {
         })}
         onIndexChange={index => this.setState({ index })}
         initialLayout={{ width: Dimensions.get('window').width }}
+        renderTabBar={renderNewTab}
       />
     );
   }
@@ -54,4 +105,18 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  tabView: {
+    backgroundColor: 'white',
+    elevation: 0
+  },
+  label: {
+    color: 'black',
+    // fontSize: Platform.OS === 'android' ? 14 : 18,
+    fontWeight: '100',
+  },
+  indicator: {
+    display: 'flex',
+    backgroundColor: 'black',
+    height: 3,
+  }
 });
